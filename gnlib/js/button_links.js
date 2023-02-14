cf48924ba1892d29733021f1b3d4b5a8ad122c6d
Guthen <33220603+Guthen@users.noreply.github.com>
2023-02-14 12:03:05 +0100

    [fixed] links & texts

--- gnlib/js/button_links.js
+++ gnlib/js/button_links.js
@@ -6,7 +6,7 @@ const button_links = new Vue( {
             ["Wiki"]: "wiki/index.html",
             ["Docmaker"]: "docmaker.html",
             ["Github"]: "https://github.com/Nogitsu/GNLib",
-            ["Discord"]: "https://discord.gg/U425mC",
+            ["Discord"]: "https://discord.gg/eKvuFhX",
         }
     }
 } )
--- gnlib/js/list_creators.js
+++ gnlib/js/list_creators.js
@@ -16,18 +16,13 @@ const list_creators = new Vue( {
             {
                 name: "Ferst- Dimitri",
                 role: "Website Creator",
-                image: "https://cdn.discordapp.com/avatars/324645432493867008/e35433c41ef8a50bdbf7ac22c159abf9.png?size=256",
+                image: "https://avatars.githubusercontent.com/u/50033147?v=4",
             },
             {
                 name: "Fleoxi",
                 role: "Archivist",
                 image: "https://avatars2.githubusercontent.com/u/43684766?s=460&v=4",
             },
-            {
-                name: "GCA",
-                role: "Sponsor",
-                image: "https://cdn.discordapp.com/icons/223070469148901376/a_3cdb53884856316a92929e60eb164dc4.webp?size=256"
-            }
         ]
     }
 } )
